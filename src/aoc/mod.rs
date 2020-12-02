50cf7415b5377f541fe85c5c87c6c00b8357ffea
Chris Sinjakli <chris@sinjakli.co.uk>
2020-12-02 02:40:27 +0000

    Experiment with more functional approach to 1a

--- src/aoc/mod.rs
+++ src/aoc/mod.rs
@@ -23,6 +23,33 @@ pub fn day1a(inputs: &[String]) -> String {
     "error".to_string()
 }
 
+pub fn day1a_functional(inputs: &[String]) -> Option<String> {
+    return inputs.
+        into_iter().
+        fold(Some(vec![]), |acc, s| match acc {
+            Some(a) => {
+                match s.parse::<i32>() {
+                    Ok(i) => Some([a, vec![i]].concat()),
+                    Err(_) => None
+                }
+            },
+            None => None
+        }).map(|amounts| {
+            let mut lookup = HashSet::new();
+
+            for amount in amounts.iter() {
+                let desired = 2020 - amount;
+
+                if lookup.contains(&desired) {
+                    return Some(format!("{}", amount * desired));
+                }
+
+                lookup.insert(amount);
+            }
+            return None;
+        }).flatten();
+}
+
 pub fn day1b(inputs: &[String]) -> String {
     // TODO: Handle errors, don't just unwrap and hope for the best
     let amounts: Vec<i32> = inputs.into_iter().map(|x| x.parse().unwrap()).collect();
