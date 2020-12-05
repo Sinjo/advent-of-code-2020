f030d52c895a2aecc506b32c49b8e2b2cb3fae1d
Chris Sinjakli <chris@sinjakli.co.uk>
2020-12-05 18:03:21 +0000

    Refactor day 5 even more

--- src/aoc/day5.rs
+++ src/aoc/day5.rs
@@ -1,20 +1,12 @@
 pub fn day5a(inputs: &[String]) -> anyhow::Result<String> {
-    let seat_ids: Vec<_> = inputs.into_iter().map(|seat| {
-        let binary = seat.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1");
-
-        usize::from_str_radix(&binary, 2).unwrap()
-    }).collect();
+    let seat_ids: Vec<_> = inputs.into_iter().map(seat_to_int).collect();
     let max = seat_ids.into_iter().max().unwrap();
 
     Ok(max.to_string())
 }
 
 pub fn day5b(inputs: &[String]) -> anyhow::Result<String> {
-    let mut seat_ids: Vec<_> = inputs.into_iter().map(|seat| {
-        let binary = seat.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1");
-
-        usize::from_str_radix(&binary, 2).unwrap()
-    }).collect();
+    let mut seat_ids: Vec<_> = inputs.into_iter().map(seat_to_int).collect();
     seat_ids.sort();
 
     let my_seat = seat_ids.windows(2).find_map(|seats| {
@@ -27,4 +19,10 @@ pub fn day5b(inputs: &[String]) -> anyhow::Result<String> {
     }).unwrap();
 
     Ok(my_seat.to_string())
+}
+
+fn seat_to_int(seat: &String) -> usize {
+    let binary = seat.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1");
+
+    usize::from_str_radix(&binary, 2).unwrap()
 }
