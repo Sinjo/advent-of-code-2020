0946dc8e97c256c6c281f1537924c116bbf0af50
Chris Sinjakli <chris@sinjakli.co.uk>
2020-12-07 22:40:25 +0000

    Extract build_rules function for day 7

--- src/aoc/day7.rs
+++ src/aoc/day7.rs
@@ -2,35 +2,7 @@ use std::collections::HashMap;
 use regex::Regex;
 
 pub fn day7a(inputs: &[String]) -> anyhow::Result<String> {
-    let initial_pattern = Regex::new(r"^(?P<container>(\w|\s)+) bags contain (?P<contents>(\w|\s|,)+)\.$").unwrap();
-    let contents_pattern = Regex::new(r"(?P<count>\d+) (?P<colour>(\w|\s)+) bags?$").unwrap();
-
-    let captured: Vec<_> = inputs.iter().map(|i| {
-        initial_pattern.captures(i).unwrap()
-    }).collect();
-
-    let mut rules = HashMap::new();
-
-    for rule in captured.iter() {
-        let container = rule.name("container").unwrap().as_str();
-        let unsplit_contents = rule.name("contents").unwrap().as_str();
-        let split_contents: Vec<_> = unsplit_contents.split(", ").collect();
-
-        let mut contents = Vec::new();
-
-        for s in split_contents.iter() {
-            if !(s == &"no other bags") {
-                let parsed_contents = contents_pattern.captures(s).unwrap();
-                let count: usize = parsed_contents.name("count").unwrap().as_str().parse().unwrap();
-                let colour = parsed_contents.name("colour").unwrap().as_str();
-
-                contents.push((colour, count));
-            }
-
-        };
-
-        rules.insert(container, contents);
-    }
+    let rules = build_rules(inputs);
 
     let shiny_gold_containers: Vec<_> = rules.keys().filter_map(|container| {
         if eventually_contains_shiny_gold(&rules, &container) {
@@ -45,35 +17,7 @@ pub fn day7a(inputs: &[String]) -> anyhow::Result<String> {
 }
 
 pub fn day7b(inputs: &[String]) -> anyhow::Result<String> {
-    let initial_pattern = Regex::new(r"^(?P<container>(\w|\s)+) bags contain (?P<contents>(\w|\s|,)+)\.$").unwrap();
-    let contents_pattern = Regex::new(r"(?P<count>\d+) (?P<colour>(\w|\s)+) bags?$").unwrap();
-
-    let captured: Vec<_> = inputs.iter().map(|i| {
-        initial_pattern.captures(i).unwrap()
-    }).collect();
-
-    let mut rules = HashMap::new();
-
-    for rule in captured.iter() {
-        let container = rule.name("container").unwrap().as_str();
-        let unsplit_contents = rule.name("contents").unwrap().as_str();
-        let split_contents: Vec<_> = unsplit_contents.split(", ").collect();
-
-        let mut contents = Vec::new();
-
-        for s in split_contents.iter() {
-            if !(s == &"no other bags") {
-                let parsed_contents = contents_pattern.captures(s).unwrap();
-                let count: usize = parsed_contents.name("count").unwrap().as_str().parse().unwrap();
-                let colour = parsed_contents.name("colour").unwrap().as_str();
-
-                contents.push((colour, count));
-            }
-
-        };
-
-        rules.insert(container, contents);
-    }
+    let rules = build_rules(inputs);
 
     let bag_count = contained_bag_count(&rules, "shiny gold");
 
@@ -107,4 +51,38 @@ fn contained_bag_count(rules: &HashMap<&str, Vec<(&str, usize)>>, container: &st
     }
 
     total
+}
+
+pub fn build_rules(inputs: &[String]) -> HashMap<&str, Vec<(&str, usize)>> {
+    let initial_pattern = Regex::new(r"^(?P<container>(\w|\s)+) bags contain (?P<contents>(\w|\s|,)+)\.$").unwrap();
+    let contents_pattern = Regex::new(r"(?P<count>\d+) (?P<colour>(\w|\s)+) bags?$").unwrap();
+
+    let captured: Vec<_> = inputs.iter().map(|i| {
+        initial_pattern.captures(i).unwrap()
+    }).collect();
+
+    let mut rules = HashMap::new();
+
+    for rule in captured.iter() {
+        let container = rule.name("container").unwrap().as_str();
+        let unsplit_contents = rule.name("contents").unwrap().as_str();
+        let split_contents: Vec<_> = unsplit_contents.split(", ").collect();
+
+        let mut contents = Vec::new();
+
+        for s in split_contents.iter() {
+            if !(s == &"no other bags") {
+                let parsed_contents = contents_pattern.captures(s).unwrap();
+                let count: usize = parsed_contents.name("count").unwrap().as_str().parse().unwrap();
+                let colour = parsed_contents.name("colour").unwrap().as_str();
+
+                contents.push((colour, count));
+            }
+
+        };
+
+        rules.insert(container, contents);
+    }
+
+    rules
 }
