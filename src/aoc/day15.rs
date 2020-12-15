5bdb722a976705df3e67f67bfa3993c83d4d0160
Chris Sinjakli <chris@sinjakli.co.uk>
2020-12-15 20:13:59 +0000

    Refactor day 15

--- src/aoc/day15.rs
+++ src/aoc/day15.rs
@@ -5,56 +5,32 @@ use std::collections::HashMap;
 
 pub fn day15a(inputs: &[String]) -> anyhow::Result<String> {
     let initial_numbers: Vec<usize> = inputs[0].split(",").map(|s| s.parse().unwrap()).collect();
-    let mut number_last_played = HashMap::new();
+    let answer = play_game(&initial_numbers, 2020);
 
-    let mut turn = 1;
-    // this will be overwritten with a real value while we're seeding the game state
-    let mut last_number_spoken = 0;
-
-    for i in initial_numbers.iter() {
-        last_number_spoken = *i;
-        number_last_played.insert(*i, turn);
-        turn += 1;
-    }
-
-    while turn <= 2020 {
-        let last_played = number_last_played.get(&last_number_spoken);
-
-        let next_number = match last_played {
-            Some(played) => {
-                turn - 1 - played
-            },
-            None =>  0
-        };
-
-        // if it's not there, next number is 0
-        // if it is there, next number is (current turn - 1 - turn from map)
-
-        // we update the hash this late as we need the old turn to still be
-        // there for the calculation above to work
-        number_last_played.insert(last_number_spoken, turn - 1);
-        last_number_spoken = next_number;
-        turn += 1;
-    }
-
-    Ok(last_number_spoken.to_string())
+    Ok(answer.to_string())
 }
 
 pub fn day15b(inputs: &[String]) -> anyhow::Result<String> {
     let initial_numbers: Vec<usize> = inputs[0].split(",").map(|s| s.parse().unwrap()).collect();
+    let answer = play_game(&initial_numbers, 30000000);
+
+    Ok(answer.to_string())
+}
+
+fn play_game(input: &Vec<usize>, number_of_turns: usize) -> usize {
     let mut number_last_played = HashMap::new();
 
     let mut turn = 1;
     // this will be overwritten with a real value while we're seeding the game state
     let mut last_number_spoken = 0;
 
-    for i in initial_numbers.iter() {
+    for i in input.iter() {
         last_number_spoken = *i;
         number_last_played.insert(*i, turn);
         turn += 1;
     }
 
-    while turn <= 30000000 {
+    while turn <= number_of_turns {
         let last_played = number_last_played.get(&last_number_spoken);
 
         let next_number = match last_played {
@@ -74,5 +50,5 @@ pub fn day15b(inputs: &[String]) -> anyhow::Result<String> {
         turn += 1;
     }
 
-    Ok(last_number_spoken.to_string())
+    last_number_spoken
 }
